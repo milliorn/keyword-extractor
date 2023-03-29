a32fb8e647fdc896d720f18123324688bf5c32da
milliorn <scottmilliorn@gmail.com>
2023-03-28 20:37:16 -0700

    type checking errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import { Heading, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/light-bulb.svg";
+import React from "react";
 
-// heaader
 function Header(): JSX.Element {
   return (
     <>
-      <Image src={logo} alt="logo" width={100} marginBottom="1rem" />
+      <Image src={logo as string} alt="logo" width={100} marginBottom="1rem" />
       <Heading color="white" marginBottom="1rem">
         AI Keyword Extractor
       </Heading>
       <Text fontSize={25} textAlign="center">
-        Paste in your text to extract keywords
+        Paste in your text below and we'll extract the keywords for you.
       </Text>
     </>
   );
