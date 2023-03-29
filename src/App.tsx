7c04b737a90b6e0135a6dbb0b28c5f1f6417a64a
milliorn <scottmilliorn@gmail.com>
2023-03-28 19:13:12 -0700

    new lockfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Container, Box } from "@chakra-ui/layout";
+
 // main app
 function App(): JSX.Element {
-
   return (
-<><h1>App</h1></>
-  )
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <h1>App</h1>
+      </Container>
+    </Box>
+  );
 }
 
-export default App
+export default App;
